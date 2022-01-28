a32156276922b31e3171ff790b38c28884bd17a1
Nick Dubelman <ndubelman@gmail.com>
2022-01-27 16:59:30 -0800

    basic MessageInput component

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { io } from 'socket.io-client'
+  import MessageInput from './components/MessageInput.svelte'
 
   const socket = io()
   socket.on('connect', function () {
@@ -12,7 +13,7 @@
 </script>
 
 <main>
-  <h1>Hello Borat!</h1>
+  <MessageInput />
 </main>
 
 <style>
@@ -21,12 +22,5 @@
     padding: 1em;
     max-width: none;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #2aa9c0;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 200;
   }
 </style>
---
+++ client/src/components/MessageInput.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let message = ''
+
+  function onSubmit() {
+    alert('message sent!') // FIXME: send message to server
+    message = '' // clear message
+  }
+</script>
+
+<form on:submit|preventDefault={onSubmit}>
+  <input type="text" bind:value={message} placeholder="enter a message" />
+
+  <button type="submit" disabled={message.trim() === ''}>Send</button>
+</form>
+
+<style>
+  form {
+    display: flex;
+    justify-content: center;
+  }
+
+  input {
+    width: 420px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+</style>
